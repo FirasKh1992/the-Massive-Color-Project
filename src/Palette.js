615a0d592153518291ae61c7d0c4f46882d70fea
FirasKh1992 <firask53@gmail.com>
2021-06-03 10:33:14 +0300

    Move out styles

--- src/Palette.js
+++ src/Palette.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar'
-import './Palette.css';
+
 import PaletteFooter from './PaletteFooter'
 import { withStyles } from "@material-ui/styles";
+import styles from './styles/PaletteStyles'
 
 
 
 
-const styles={
-    Palette:{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-    },
-    colors:{
-        height:"90%",
-    }
-
-}
 class Palette extends Component {
     constructor(props) {
         super(props);
