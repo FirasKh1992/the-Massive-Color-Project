3bd16abd402a8911a4ea90c6061027536dd99053
FirasKh1992 <firask53@gmail.com>
2021-06-07 19:06:29 +0300

    style Draggable colorBox

--- src/DragableColorBox.js
+++ src/DragableColorBox.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { withStyles } from '@material-ui/styles';
+import { mergeClasses, withStyles } from '@material-ui/styles';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutlined'
+import transitions from '@material-ui/core/styles/transitions';
 const styles = {
     root: {
         height: "25%",
@@ -9,14 +11,42 @@ const styles = {
         position: "relative",
         cursor: "pointer",
         marginBottom: "-3.5px",
+        "&:hover svg":{
+            color:"white",
+            transform:"scale(1.5)"
+          
+        }
+    },
+    boxContent:{
+        position: "absolute",
+        width: "100%",
+        left:" 0px",
+        bottom:" 0px",
+        padding: "10px",
+        color: "rgba(0,0,0,0.5)",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+        display:"flex",
+        justifyContent:"space-between"
+    },
+    deleteIcon:{
+       transition:"all 0.3s ease-in-out"
+
     }
 }
 function DragableColoBox(props) {
     const { classes } = props;
     return (
-        <div className={classes.root} style={{ backgroundColor: props.color }}>
-            {props.name}
+        <div
+            className={classes.root}
+            style={{ backgroundColor: props.color }}>
+            <div className={classes.boxContent}>
+               <span>{props.name}</span> 
+              <DeleteOutlineIcon className={classes.deleteIcon}/>
+            </div>
         </div>
+
     )
 }
 
