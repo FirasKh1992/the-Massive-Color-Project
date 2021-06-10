d1d6723e62e54e6de853e99949946b29d56b8494
FirasKh1992 <firask53@gmail.com>
2021-06-10 16:02:16 +0300

    add background svg to paletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,54 +1,60 @@
-const styles={
+import bg from './bg.svg'
+const styles = {
     root: {
-        backgroundColor:"blue",
-        height:"100vh",
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center"
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#1d24aa",
+        backgroundImage: `url(${bg})`, /* background by SVGBackgrounds.com */
+        overflow: "scroll"
     },
-    container:{
-        width:"50%",
-        display:"flex",
-        alignItems:"flex-start",
-        flexDirection:"column",
-        flexWrap:"wrap",
+    heading: {
+        fontSize: "2rem"
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
         "@media (max-width: 1600px)": {
             width: "80%",
         },
         "@media (max-width: 575.98px)": {
             width: "70%",
-            
+
         },
 
 
     },
-    nav:{
-        display:"flex",
-        width:"100%",
-        justifyContent:"space-between",
-        color:"white",
-        alignItems:"center",
-        "& a":{
-            color:"white"
+    nav: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between",
+        color: "white",
+        alignItems: "center",
+        "& a": {
+            color: "white"
         }
 
     },
-    palettes:{
-        boxSizing:"border-box",
-        width:"100%",
-        display:"grid",
-        gridTemplateColumns:"repeat(3,30%)",
-        gridGap:"2.5rem",
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3,30%)",
+        gridGap: "2.5rem",
         "@media (max-width: 767.98px)": {
-            gridTemplateColumns:"repeat(2,50%)",
-           
-            
+            gridTemplateColumns: "repeat(2,50%)",
+
+
         },
         "@media (max-width: 575.98px)": {
-            gridTemplateColumns:"repeat(1,100%)",
-            gridGap:"1rem"
-           
-            
+            gridTemplateColumns: "repeat(1,100%)",
+            gridGap: "1rem"
+
+
         },
 
     }
